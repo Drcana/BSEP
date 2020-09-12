f4c4cb9f1a05361a364196cd9a45f3ac5aa23e00
drcana <jelenicaad@gmail.com>
2020-09-12 17:58:17 +0200

    remove print

--- ocsp/app.py
+++ ocsp/app.py
@@ -15,7 +15,5 @@ def configure(app):
     app.config.from_pyfile('config.py')
     app.config.from_pyfile(os.environ.get('CONFIG_FILE', ''), silent=True)
 
-    print(app.config)
-
 app = create_app()
 
